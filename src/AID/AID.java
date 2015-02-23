8575b1a70c82bca973c64dc3b8248d639da4d1af
Dmytro.Svarychevskyi <Dmytro.Svarychevskyi@in4mates.com>
2015-02-23 13:21:03 +0100

    генерування словників з конфігурації

--- src/AID/AID.java
+++ src/AID/AID.java
@@ -1,14 +1,9 @@
 package AID;
 
-import AID.form.CommandTreeForm;
 import AID.form.MainForm;
-import AID.form.SystemPropForm;
-import AID.io.IOOperation;
 import AID.voice.StartRecognize;
 import AID.voice.Synthesizer;
 import org.apache.log4j.Logger;
-
-import java.util.*;
 
 public class AID {
     private static final Logger logger = Logger.getLogger(AID.class);
@@ -16,9 +11,13 @@ public class AID {
         Synthesizer.speak(" AID system initializing...");
         logger.info("AID system initializing...");
         new MainForm();
-//        new CommandTreeForm();
+
         new StartRecognize();
 
+//        new CommandTreeForm();
+//        HashMap<String, List<String>> basicConfig=IOOperation.loadProperties("src/resources/configuration/Basic.config");
+//        GenerateJSGF g = new GenerateJSGF();
+//        g.generate(basicConfig);
 
 //        HashMap<String, List<String>> sysProp=IOOperation.loadProperties("src/resources/configuration/System.properties");
 //        System.out.println(sysProp.get("systType").get(0).toString());
--- src/AID/io/GenerateJSGF.java
+++ src/AID/io/GenerateJSGF.java
@@ -1,7 +1,77 @@
 package AID.io;
 
+import org.apache.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by dima-sv on 23.02.15.
  */
 public class GenerateJSGF {
+    static String version="V1.0";
+    static String name="mainAID";
+    static String annotate="automatic generating by AID-Systems";
+    private static final Logger logger = Logger.getLogger(GenerateJSGF.class);
+
+    public static void generate(HashMap<String, List<String>> map){
+        HashMap mapListType = new HashMap();
+        HashMap mapListBuf = new HashMap();
+        HashMap mapListCommand = new HashMap();
+
+        for (String key : map.keySet()) {
+            mapListType.put(map.get(key).get(0).toString(),map.get(key).get(0).toString());
+        }
+        for (String key : map.keySet()) {
+            mapListCommand.put(key,map.get(key).get(1).toString());
+        }
+        for (String key : map.keySet()) {
+            mapListBuf.put(key, map.get(key).get(0).toString());
+        }
+        try {
+            File file = new File("src/resources/mainAID.gram");
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    logger.error(e);
+                }
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("#JSGF "+version+";");
+            bw.newLine();
+            bw.write("grammar "+name+";");
+            bw.newLine();
+            bw.write("/* "+annotate+" */");
+            bw.newLine();
+            for (Object typeKey : mapListType.keySet()) {
+                bw.write("public <" + mapListType.get(typeKey).toString() + "> = ");
+                    for (Object commandKey : mapListCommand.keySet()) {
+                    if (typeKey==mapListBuf.get(commandKey)) {
+                        bw.write(mapListCommand.get(commandKey).toString());
+                        bw.write(" | ");
+                    }
+                }
+                bw.write("END of definition line");
+                bw.write(";");
+                bw.newLine();
+            }
+
+            bw.close();
+
+            System.out.println("Done!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+    }
 }
